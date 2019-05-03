f251e0a2f761a564b2fd093849dee960e0eccafc
waltergu <waltergu1989@gmail.com>
2019-05-03 23:52:32 +0800

    First CI test.

---
+++ docs/make.jl
@@ -0,0 +1,26 @@
+push!(LOAD_PATH,"../src/")
+
+using Documenter
+using DocumenterTools
+using ExactDiagonalization
+
+makedocs(
+    format=     Documenter.HTML(
+                    prettyurls= !("local" in ARGS),
+                    canonical=  "https://quantum-many-body.github.io/ExactDiagonalization.jl/latest/",
+                    assets=     ["assets/favicon.ico"],
+                    analytics=  "UA-89508993-1",
+                    ),
+    clean=      false,
+    sitename=   "ExactDiagonalization.jl",
+    pages=      [
+                "Home"      =>  "index.md",
+                ]
+)
+
+deploydocs(
+    repo=       "github.com/Quantum-Many-Body/ExactDiagonalization.jl.git",
+    target=     "build",
+    deps=       nothing,
+    make=       nothing,
+)
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using Test
+
+@testset "all" begin
+
+end
