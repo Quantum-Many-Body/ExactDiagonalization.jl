5cfb47d39f626df5e0ad34b0226eb1f819340dad
waltergu <waltergu1989@gmail.com>
2021-05-25 21:45:31 +0800

    Compatible with QuantumLatticesv0.2.0.

--- src/FED.jl
+++ src/FED.jl
@@ -1,15 +1,16 @@
 module FED
 
-using Printf: @printf,@sprintf
-using SparseArrays: SparseMatrixCSC,spzeros
-using QuantumLattices: FockOperator,Operators,FOperator,MAJORANA,CREATION,rank
+using Printf: @printf, @sprintf
+using SparseArrays: spzeros
+using QuantumLattices: AbstractFockOperator, Operators, FOperator, CREATION, rank
 using QuantumLattices.Prerequisites: Float
 using QuantumLattices.Mathematics.VectorSpaces: VectorSpace
 
-import QuantumLattices: kind,id,dimension,matrix
+import SparseArrays: SparseMatrixCSC
+import QuantumLattices: kind, id, dimension
 import QuantumLattices.Mathematics.AlgebraOverFields: idtype
 
-export FockBasis,GBasis
+export FockBasis, GBasis, PBasis, SBasis, SPBasis
 
 """
     FockBasis{I<:Unsigned} <: VectorSpace{I}
@@ -17,15 +18,16 @@ export FockBasis,GBasis
 Abatract type for all kinds of Fock basis.
 """
 abstract type FockBasis{I<:Unsigned} <: VectorSpace{I} end
-kind(bs::FockBasis)=bs|>typeof|>kind
-idtype(bs::FockBasis)=bs|>typeof|>idtype
-function Base.show(io::IO,bs::FockBasis)
+@inline kind(bs::FockBasis) = kind(typeof(bs))
+@inline idtype(bs::FockBasis) = idtype(typeof(bs))
+function Base.show(io::IO, bs::FockBasis)
     @printf io "%s:\n" repr(bs)
-    for i=1:dimension(bs)
-        @printf io "  %s\n" string(bs[i],base=2)
+    for i = 1:dimension(bs)
+        @printf io "  %s\n" string(bs[i], base=2)
     end
 end
-Base.repr(bs::FockBasis)=@sprintf "%s(%s)" nameof(typeof(bs)) join(id(bs),",")
+@inline Base.repr(bs::FockBasis) = @sprintf "%s(%s)" nameof(typeof(bs)) join(id(bs), ",")
+@inline Base.issorted(bs::FockBasis) = true
 
 """
     GBasis{I}(nstate::Int) where I<:Unsigned
@@ -35,48 +37,137 @@ Generic fermionic/hard-core-bosonic basis, which uses no conserved quantities.
 struct GBasis{I<:Unsigned} <: FockBasis{I}
     nstate::Int
 end
-dimension(bs::GBasis)=2^bs.nstate
-Base.getindex(bs::GBasis,i::Int)=i-1
-Base.searchsortedfirst(bs::GBasis,i::Int)=i+1
-kind(::Type{<:GBasis})=:g
-idtype(::Type{<:GBasis})=Int
-id(bs::GBasis)=bs.nstate
+@inline kind(::Type{<:GBasis}) = :g
+@inline idtype(::Type{<:GBasis}) = Int
+@inline id(bs::GBasis) = bs.nstate
+@inline dimension(bs::GBasis) = 2^bs.nstate
+@inline Base.getindex(bs::GBasis, i::Int) = i-1
+@inline Base.searchsortedfirst(bs::GBasis, i::Int) = i+1
 
 """
-    matrix(operator::FockOperator,basis::FockBasis) -> SparseMatrixCSC{valtype(operator),Int}
-    matrix(opts::Operators,basis::FockBasis,dtype::Type{<:Number}=Complex{Float}) -> SparseMatrixCSC{dtype,Int}
-
-Get the CSC-formed sparse matrix representation of an operator / a set of operators.
 """
-function matrix(operator::FockOperator,basis::FockBasis)
-    @assert rank(operator)%2==0 "matrix error: two bases are needed for odd ranked Fock operators."
-    ndata,data,indices,indptr=1,zeros(valtype(operator),dimension(basis)),zeros(Int,dimension(basis)),zeros(Int,dimension(basis)+1)
-    eye,temp,flag=one(basis|>eltype),zeros(basis|>eltype,rank(operator)+1),true
-    seqs=NTuple{rank(operator),Int}(seq-1 for seq in operator.id.seqs)
-    nambus=NTuple{rank(operator),Bool}((oid.index.nambu==MAJORANA ? error("matrix error: majorana operator not supported.") : oid.index.nambu==CREATION) for oid in operator.id)
-    for i=1:dimension(basis)
-        flag=true
-        indptr[i]=ndata
-        temp[1]=basis[i]
-        for j=1:rank(operator)
-            (temp[j]&eye<<seqs[j]>0)==nambus[j] && (flag=false; break)
-            temp[j+1]=nambus[j] ? temp[j]|eye<<seqs[j] : temp[j]&~(eye<<seqs[j])
+struct PBasis{I<:Unsigned} <: FockBasis{I}
+    nstate::Int
+    nparticle::Int
+    table::Vector{I}
+end
+@inline kind(::Type{<:PBasis}) = :p
+@inline idtype(::Type{<:PBasis}) = Tuple{Int, Int}
+@inline id(bs::PBasis) = (bs.nstate, bs.nparticle)
+@inline dimension(bs::PBasis) = length(bs.table)
+@inline Base.getindex(bs::GBasis, i::Int) = bs.table[i]
+
+"""
+"""
+struct SBasis{I<:Unsigned} <: FockBasis{I}
+    nstate::Int
+    spinz::Rational{Int}
+    table::Vector{I}
+end
+@inline kind(::Type{<:SBasis}) = :s
+@inline idtype(::Type{<:SBasis}) = Tuple{Int,Rational{Int}}
+@inline id(bs::SBasis) = (bs.nstate, bs.spinz)
+@inline dimension(bs::SBasis) = length(bs.table)
+@inline Base.getindex(bs::SBasis, i::Int) = bs.table[i]
+
+"""
+"""
+struct SPBasis{I<:Unsigned} <: FockBasis{I}
+    nstate::Int
+    nparticle::Int
+    spinz::Rational{Int}
+    table::Vector{I}
+end
+@inline kind(::Type{<:SPBasis}) = :sp
+@inline idtype(::Type{<:SPBasis}) = Tuple{Int, Int, Rational{Int}}
+@inline id(bs::SPBasis) = (bs.nstate, bs.nparticle, bs.spinz)
+@inline dimension(bs::SPBasis) = length(bs.table)
+@inline Base.getindex(bs::SPBasis, i::Int) = bs.table[i]
+
+"""
+    SparseMatrixCSC(operator::AbstractFockOperator, basis::FockBasis, table) -> SparseMatrixCSC{valtype(operator), Int}
+    SparseMatrixCSC(operator::AbstractFockOperator, basis₁::FockBasis, basis₂::FockBasis, table) -> SparseMatrixCSC{valtype(operator), Int}
+    SparseMatrixCSC(opts::Operators, basis::FockBasis, table, dtype::Type{<:Number}=Complex{Float}) -> SparseMatrixCSC{dtype, Int}
+    SparseMatrixCSC(opts::Operators, basis₁::FockBasis, basis₂::FockBasis, table, dtype::Type{<:Number}=Complex{Float}) -> SparseMatrixCSC{dtype, Int}
+
+Get the CSC-formed sparse matrix representation of
+1) an even-ranked operator,
+2) an odd-ranked operator,
+3) a set of even-ranked operators,
+4) a set of odd-ranked operators.
+"""
+function SparseMatrixCSC(operator::AbstractFockOperator, basis::FockBasis, table)
+    @assert rank(operator)%2==0 "SparseMatrixCSC error: two bases are needed for odd ranked Fock operators."
+    ndata, data, indices, indptr = 1, zeros(valtype(operator), dimension(basis)), zeros(Int, dimension(basis)), zeros(Int, dimension(basis)+1)
+    eye, temp, flag = one(basis|>eltype), zeros(basis|>eltype, rank(operator)+1), true
+    seqs = NTuple{rank(operator), Int}(table[id(operator)[i]]-1 for i = 1:rank(operator))
+    nambus = NTuple{rank(operator), Bool}(oid.index.nambu==CREATION for oid in id(operator))
+    for i = 1:dimension(basis)
+        flag = true
+        indptr[i] = ndata
+        temp[1] = basis[i]
+        for j = 1:rank(operator)
+            (temp[j]&eye<<seqs[j]>0)==nambus[j] && (flag = false; break)
+            temp[j+1] = nambus[j] ? temp[j]|eye<<seqs[j] : temp[j]&~(eye<<seqs[j])
         end
         if flag
-            nsign=0
-            isa(operator,FOperator) && for j=1:rank(operator) for k=0:seqs[j]-1 (temp[j]&eye<<k>0 && (nsign+=1)) end end
-            indices[ndata]=searchsortedfirst(basis,temp[end])
-            data[ndata]=(-1)^nsign*operator.value
-            ndata+=1
+            nsign = 0
+            isa(operator, FOperator) && for j = 1:rank(operator)
+                for k = 0:seqs[j]-1
+                    temp[j]&eye<<k>0 && (nsign += 1)
+                end
+            end
+            indices[ndata] = searchsortedfirst(basis, temp[end])
+            data[ndata] = operator.value*(-1)^nsign
+            ndata += 1
         end
     end
-    indptr[end]=ndata+1
-    return SparseMatrixCSC(dimension(basis),dimension(basis),indptr,indices[1:ndata],data[1:ndata])
+    indptr[end] = ndata+1
+    return SparseMatrixCSC(dimension(basis), dimension(basis), indptr, indices[1:ndata], data[1:ndata])
 end
-function matrix(opts::Operators,basis::FockBasis,dtype::Type{<:Number}=Complex{Float})
-    result=spzeros(dtype,dimension(basis),dimension(basis))
-    for opt in values(opts) result=result+matrix(opt,basis) end
+function SparseMatrixCSC(operator::AbstractFockOperator, basis₁::FockBasis, basis₂::FockBasis, table)
+    @assert rank(operator)%2==1 "SparseMatrixCSC error: only one basis is needed for even ranked Fock operators."
+    ndata, data, indices, indptr = 1, zeros(valtype(operator), dimension(basis₁)), zeros(Int, dimension(basis₁)), zeros(Int, dimension(basis₁)+1)
+    eye, temp, flag = one(basis₁|>eltype), zeros(basis₁|>eltype, rank(operator)+1), true
+    seqs = NTuple{rank(operator), Int}(table[id(operator)id[i]] for i = 1:rank(operator))
+    nambus = NTuple{rank(operator), Bool}(oid.index.nambu==CREATION for oid in id(operator))
+    for i = 1:dimension(basis₁)
+        flag = true
+        indptr[i] = ndata
+        temp[1] = basis₁[i]
+        for j = 1:rank(operator)
+            (temp[j]&eye<<seqs[j]>0)==nambus[j] && (flag = false; break)
+            temp[j+1] = nambus[j] ? temp[j]|eye<<seqs[j] : temp[j]&~(eye<<seqs[j])
+        end
+        if flag
+            nsign = 0
+            isa(operator,FOperator) && for j = 1:rank(operator)
+                for k = 0:seqs[j]-1
+                    temp[j]&eye<<k>0 && (nsign += 1)
+                end
+            end
+            indices[ndata] = searchsortedfirst(basis₂, temp[end])
+            data[ndata] = operator.value*(-1)^nsign
+            ndata += 1
+        end
+    end
+    indptr[end] = ndata+1
+    return SparseMatrixCSC(dimension(basis₂), dimension(basis₁), indptr, indices[1:ndata], data[1:ndata])
+end
+function SparseMatrixCSC(opts::Operators, basis::FockBasis, table, dtype::Type{<:Number}=Complex{Float})
+    result = spzeros(dtype, dimension(basis), dimension(basis))
+    for opt in values(opts)
+        result += SparseMatrixCSC(opt, basis, table)
+    end
     return result
 end
+function SparseMatrixCSC(opts::Operators, basis₁::FockBasis, basis₂::FockBasis, table, dtype::Type{<:Number}=Complex{Float})
+    result = spzeros(dtype, dimension(basis₂), dimension(basis₁))
+    for opt in values(opts)
+        result += matrix(opt, basis₁, basis₂, table)
+    end
+    return result
+end
+
 
 end  # module
--- test/FED.jl
+++ test/FED.jl
@@ -1,29 +1,30 @@
 using Test
-using ExactDiagonalization.FED
-using QuantumLattices
-import QuantumLattices.Mathematics.AlgebraOverFields: idtype
 using Arpack: eigs
+using ExactDiagonalization.FED
+using SparseArrays: SparseMatrixCSC
+using QuantumLattices
+using QuantumLattices.Mathematics.AlgebraOverFields: idtype
 
 @testset "GBasis" begin
-    basis=GBasis{UInt}(2)
-    @test basis|>kind==basis|>typeof|>kind==:g
-    @test basis|>idtype==basis|>typeof|>idtype==Int
-    @test dimension(basis)==2^2
-    @test id(basis)==2
-    @test basis[4]==3
-    @test searchsortedfirst(basis,3)==4
-    @test string(basis)=="GBasis(2):\n  0\n  1\n  10\n  11\n"
-    @test repr(basis)=="GBasis(2)"
+    basis = GBasis{UInt}(2)
+    @test kind(basis) == kind(typeof(basis)) == :g
+    @test idtype(basis) == idtype(typeof(basis)) == Int
+    @test dimension(basis) == 2^2
+    @test id(basis) == 2
+    @test basis[4] == 3
+    @test searchsortedfirst(basis, 3) == 4
+    @test string(basis) == "GBasis(2):\n  0\n  1\n  10\n  11\n"
+    @test repr(basis) == "GBasis(2)"
 end
 
-@testset "matrix" begin
-    lattice=Lattice("L2P",[Point(PID(1),(0.0,)),Point(PID(2),(1.0,))])
-    config=IDFConfig{Fock}(pid->Fock(atom=pid.site%2,norbital=1,nspin=2,nnambu=2),lattice.pids)
-    table=Table(config,usualfockindextotuple)
-    t=Hopping{'F'}(:t,1.0,1)
-    U=Hubbard{'F'}(:U,1.0)
-    μ=Onsite{'F'}(:μ,-0.5)
-    g=Generator((t,U,μ),Bonds(lattice),config,table,false)
-    m=matrix(expand(g),GBasis{UInt}(4))
-    @test eigs(m,which=:SR,ritzvec=false)[1][1]≈-2.5615528128088303
+@testset "SparseMatrixCSC" begin
+    lattice = Lattice("L2P", [Point(PID(1), [0.0]), Point(PID(2), [1.0])])
+    config = Config{Fock{:f}}(pid->Fock{:f}(atom=pid.site%2, norbital=1, nspin=2, nnambu=2), lattice.pids)
+    table = Table(config, usualfockindextotuple)
+    t = Hopping(:t, 1.0, 1)
+    U = Hubbard(:U, 1.0)
+    μ = Onsite(:μ, -0.5)
+    g = Generator((t, U, μ), Bonds(lattice), config, half=false, table=table)
+    m = SparseMatrixCSC(expand(g), GBasis{UInt}(4), g.table)
+    @test eigs(m, which=:SR, ritzvec=false)[1][1] ≈ -2.5615528128088303
 end
