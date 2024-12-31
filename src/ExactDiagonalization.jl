94c2d4b0afe1893802c4b17df2079b11e84e0cd8
waltergu <waltergu1989@gmail.com>
2024-12-31 16:09:33 +0800

    Rename `EDCore` to `Core`.

--- src/ExactDiagonalization.jl
+++ src/ExactDiagonalization.jl
@@ -1,8 +1,8 @@
 module ExactDiagonalization
 
 # Core
-include("EDCore.jl")
-export edtimer, ED, EDEigen, EDKind, EDMatrix, EDMatrixization, Sector, SectorFilter, TargetSpace, productable, release!, sumable
+include("Core.jl")
+export edtimer, ED, EDEigen, EDKind, EDMatrix, EDMatrixization, Sector, SectorFilter, TargetSpace, eigen, productable, release!, sumable
 export BinaryBases, BinaryBasis, BinaryBasisRange, basistype
 export AbelianBases
 # export xyz2ang, spincoherentstates, structure_factor, Pspincoherentstates
