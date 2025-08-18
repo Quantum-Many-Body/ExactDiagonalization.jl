a6eb5df61c79d7ed51d9e2f8dc6d21e4ff428343
waltergu <waltergu1989@gmail.com>
2025-08-18 12:59:57 +0800

    `ED.basistype` and `ED.BinaryBasisRange` are no longer exported.

--- src/ExactDiagonalization.jl
+++ src/ExactDiagonalization.jl
@@ -2,8 +2,8 @@ module ExactDiagonalization
 
 # Core
 include("Core.jl")
-export edtimer, ED, EDEigen, EDEigenData, EDKind, EDMatrix, EDMatrixization, Sector, SectorFilter, eigen, prepare!, productable, release!, sumable
-export AbelianBases, BinaryBases, BinaryBasis, BinaryBasisRange, basistype
-export GroundStateExpectation, GroundStateExpectationData, StaticTwoPointCorrelator, StaticTwoPointCorrelatorData
+export edtimer, eigen, prepare!, productable, release!, sumable
+export AbelianBases, BinaryBases, BinaryBasis, ED, EDKind, EDMatrix, EDMatrixization, Sector, SectorFilter
+export EDEigen, EDEigenData, GroundStateExpectation, GroundStateExpectationData, StaticTwoPointCorrelator, StaticTwoPointCorrelatorData
 
 end # module
